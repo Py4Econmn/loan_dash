94e74784052cc4c543e8695ff3d587e70d3485d9
Py4Econmn <58402008+Py4Econmn@users.noreply.github.com>
2024-04-27 14:15:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ profile_pic = current_dir / "assets" / "profile-pic.png"
 # --- GENERAL SETTINGS ---
 PAGE_TITLE = "Digital CV | John Doe"
 PAGE_ICON = ":wave:"
-NAME = "John Doe"
+NAME = "OtgonJargal"
 DESCRIPTION = """
 Senior Data Analyst, assisting enterprises by supporting data-driven decision-making.
 """
